0216650f090c727e36156534e26c70c46336c66e
Cleyson Lima <cleysonph@gmail.com>
2023-07-09 22:31:37 -0300

    04:07 - Criando o serializer de Teacher

---
+++ teachers/serializers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+
+from .models import Teacher
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(
+        required=True, min_length=3, max_length=100, source="name"
+    )
+    idade = serializers.IntegerField(
+        required=True, min_value=18, max_value=100, source="age"
+    )
+    descricao = serializers.CharField(
+        required=True, min_length=10, max_length=500, source="description"
+    )
+    password_confirmation = serializers.CharField(write_only=True)
+    valor_hora = serializers.DecimalField(required=True, min_value=10, max_value=500)
+
+    class Meta:
+        model = Teacher
+        field = (
+            "nome",
+            "email",
+            "idade",
+            "descricao",
+            "valor_hora",
+            "created_at",
+            "updated_at",
+            "password",
+            "password_confirmation",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "created_at": {"read_only": True},
+            "updated_at": {"read_only": True},
+        }
+
+    def validate_password_confirmation(self, value):
+        if self.initial_data["password"] != value:
+            raise serializers.ValidationError("as senha não são iguais")
+        return value
+
+    def create(self, validated_data):
+        del validated_data["password_confirmation"]
+        validated_data["password"] = make_password(validated_data["password"])
+        super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        del validated_data["password_confirmation"]
+        validated_data["password"] = make_password(validated_data["password"])
+        return super().update(instance, validated_data)
