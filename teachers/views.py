e0a731e46f2897b4c0eb96930bdf3a15306c7293
Cleyson Lima <cleysonph@gmail.com>
2023-07-09 23:09:09 -0300

    05:09 - Implementando a rota de busca de professores

--- teachers/views.py
+++ teachers/views.py
@@ -7,6 +7,12 @@ from .serializers import TeacherSerializer
 
 
 class TeacherList(APIView):
+    def get(self, request):
+        q = request.query_params.get("q", "")
+        teachers = Teacher.objects.filter(description__icontains=q)
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         serializer = TeacherSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
